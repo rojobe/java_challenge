f88aba629b01d46647ab19417375294f9c5a180d
Rodrigo Benito <rodrigobenito33@gmail.com>
2023-03-21 14:54:20 -0300

    password regex updated

--- src/main/java/com/java/challenge/java_challenge/service/RegularExpresionService.java
+++ src/main/java/com/java/challenge/java_challenge/service/RegularExpresionService.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public interface RegularExpresionService {
 
-    String PASS_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,}$"; //example: a2asfGf6
+    String PASS_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,12}$"; //example: a2asfGf6
     String EMAIL_REGEX = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
 
     boolean regexPassword(String password);
